f49c5f78da95dfa4b91b665e69219ad5ddcfc3b6
Farth Vader <cartmanboy1991@gmail.com>
2017-03-10 10:15:34 +0000

    Update client reqs

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./requirements.txt', session='hack')
-
-# reqs is a list of requirement
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="he-emulator-client",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=reqs,
+    install_requires=[
+        "thrift>=0.9.3",
+    ],
     include_package_data=True,
     author="Vishal Gowda",
     author_email="vishal@hackerearth.com",
